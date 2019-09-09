5e3016d2c3553418425b957ad1b8211b79790850
stephnathai <spontandom@gmail.com>
2019-09-09 16:04:43 -0400

    Updates

---
+++ precache-manifest.6f6b458850fd91e132e93708d82022d0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "812000a5ce2deaca63897ca2b2246b9c",
+    "url": "/static/media/github.812000a5.svg"
+  },
+  {
+    "revision": "e5540508b9c5901c1464",
+    "url": "/static/css/main.ae2af853.chunk.css"
+  },
+  {
+    "revision": "39b00e9b1e9705b8b4c3",
+    "url": "/static/js/1.39b00e9b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a2da4d7f39747265433a3ec566136d26",
+    "url": "/static/media/left-arrow.a2da4d7f.svg"
+  },
+  {
+    "revision": "17f95d803cb1c3a23edbb10095f8c6ee",
+    "url": "/static/media/right-arrow.17f95d80.svg"
+  },
+  {
+    "revision": "3717bb380de2db52653966c44b3b949d",
+    "url": "/static/media/main.3717bb38.jpg"
+  },
+  {
+    "revision": "cabaf93c086201e626dd25e3301b39fc",
+    "url": "/static/media/Nathai-Cueter resume.cabaf93c.pdf"
+  },
+  {
+    "revision": "39e4a17ec5d8d800dbb828ba43222ac4",
+    "url": "/static/media/email.39e4a17e.svg"
+  },
+  {
+    "revision": "e5540508b9c5901c1464",
+    "url": "/static/js/main.e5540508.chunk.js"
+  },
+  {
+    "revision": "d2cffeb831e613fbc0d145f2e992d41c",
+    "url": "/static/media/linkedin.d2cffeb8.svg"
+  },
+  {
+    "revision": "79df31db3831324e99440168e53edfb9",
+    "url": "/static/media/svg-fun.79df31db.svg"
+  },
+  {
+    "revision": "1eacf255870a3c7664bbfdd1bf4e19cb",
+    "url": "/static/media/groupaway1.1eacf255.png"
+  },
+  {
+    "revision": "185631d3690edfc159ca939fe2b5df11",
+    "url": "/static/media/groupaway2.185631d3.png"
+  },
+  {
+    "revision": "a00ba69623558c75857161addd2d0148",
+    "url": "/static/media/licwiki.a00ba696.png"
+  },
+  {
+    "revision": "56f999b100f163c9d392a326ce5482c7",
+    "url": "/static/media/trainTrack1.56f999b1.png"
+  },
+  {
+    "revision": "585baf7082dfeaf8eab5b8ea9c8aa560",
+    "url": "/static/media/trainTrack2.585baf70.png"
+  },
+  {
+    "revision": "68365445ac208eb17b4670d5db77340d",
+    "url": "/index.html"
+  }
+];
--- src/components/Header/Name.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Name({name}) {
-  return (
-    <h1 className='header-name'>{name}</h1>
-  );
-}
-
-export default Name;
